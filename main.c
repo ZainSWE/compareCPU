5241abd8930f62d6485be8aca6f82e4054e83d8d
Zain Ahmad <zainahmad.swe@gmail.com>
2025-10-19 10:08:23 -0400

    added cpu1 & cpu2 user input fields

--- main.c
+++ main.c
@@ -12,29 +12,115 @@ typedef struct CPU {
     int tdp;
 } CPU;
 
-void hashSeperator();
+void hashSeperator(int size);
 
-double calculateScore(CPU *ptr, double weight);
+double calculateScore1(CPU *cpu1, double weight);
+double calculateScore2(CPU *cpu2, double weight);
 
 int main () {
 
-    CPU cpu[1];
-    CPU *ptr = &cpu[0];
+    CPU cpu[2];
+    CPU *cpu1 = &cpu[0];
+    CPU *cpu2 = &cpu[1];
+
 
     strcpy (cpu[0].name, "null");
-    cpu[0].clock = 3.6;
-    cpu[0].l3cache = 25.0;
-    cpu[0].price = 573.52;
-    cpu[0].threads = 20;
-    cpu[0].cores = 12;
-    cpu[0].tdp = 125;
+    cpu[0].clock = 0.0;
+    cpu[0].l3cache = 0.0;
+    cpu[0].price = 0.0;
+    cpu[0].threads = 0;
+    cpu[0].cores = 0;
+    cpu[0].tdp = 0;
 
+    strcpy (cpu[1].name, "null");
+    cpu[1].clock = 0.0;
+    cpu[1].l3cache = 0.0;
+    cpu[1].price = 0.0;
+    cpu[1].threads = 0;
+    cpu[1].cores = 0;
+    cpu[1].tdp = 0;
 
+    int size = 44;
     char line[255];
     double weight;
-    double score;
-    double value;
-    double efficiency;
+    double scoreCPU1, scoreCPU2;
+    double valueCPU1, valueCPU2;
+    double efficiencyCPU1, efficiencyCPU2;
+
+    // CPU 1
+
+    printf("Enter CPU 1 name: ");
+    fgets(cpu[0].name, sizeof(cpu[0].name), stdin);
+    cpu[0].name[strcspn(cpu[0].name, "\n")] = '\0';
+
+    printf("Enter clock speed (Ghz): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[0].clock);
+
+    printf("Enter L3 cache (MB): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[0].l3cache);
+
+    printf("Enter price: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[0].price);
+
+    printf("Enter threads: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[0].threads);
+
+    printf("Enter cores: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[0].cores);
+
+    printf("Enter tdp/power (W): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[0].tdp);
+
+    do {
+        printf("Enter L3 cache weight (ranging from 0.0 to 1.0): ");
+        fgets(line, sizeof(line), stdin);
+        sscanf(line, "%lf", &weight);
+
+	if (weight > 1.0 || weight < 0.0) {
+	    hashSeperator(size);
+	    printf("# Error: weight must range from 0.0 to 1.0 #\n");
+	    hashSeperator(size);
+	}
+
+    } while (weight > 1.0 || weight < 0.0);
+
+    hashSeperator(size);
+
+    // CPU 2
+
+    printf("Enter CPU 2 name: ");
+    fgets(cpu[1].name, sizeof(cpu[1].name), stdin);
+    cpu[1].name[strcspn(cpu[1].name, "\n")] = '\0';
+
+    printf("Enter clock speed (Ghz): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[1].clock);
+
+    printf("Enter L3 cache (MB): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[1].l3cache);
+
+    printf("Enter price: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%lf", &cpu[1].price);
+
+    printf("Enter threads: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[1].threads);
+
+    printf("Enter cores: ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[1].cores);
+
+    printf("Enter tdp/power (W): ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &cpu[1].tdp);
 
     do {
 	printf("Enter L3 cache weight (ranging from 0.0 to 1.0): ");
@@ -42,36 +128,54 @@ int main () {
 	sscanf(line, "%lf", &weight);
 
 	if (weight > 1.0 || weight < 0.0) {
-	    hashSeperator();
+	    hashSeperator(size);
 	    printf("# Error: weight must range from 0.0 to 1.0 #\n");
-	    hashSeperator();
+	    hashSeperator(size);
 	}
 
     } while (weight > 1.0 || weight < 0.0);
 
-    score = calculateScore(ptr, weight);
-    value = score / cpu[0].price;
-    efficiency = score / cpu[0].tdp;
+    scoreCPU1 = calculateScore1(cpu1, weight);
+    valueCPU1 = scoreCPU1 / cpu[0].price;
+    efficiencyCPU1 = scoreCPU1 / cpu[0].tdp;
 
-    hashSeperator();
-    printf("score: %.2lf\n", score);
-    printf("value: %.2lf\n", value);
-    printf("efficiency: %.2lf\n", efficiency);
-    hashSeperator();
+    scoreCPU2 = calculateScore2(cpu2, weight);
+    valueCPU2 = scoreCPU2 / cpu[1].price;
+    efficiencyCPU2 = scoreCPU2 / cpu[1].tdp;
+
+    hashSeperator(size);
+    printf("CPU: %s\n", cpu[0].name);
+    printf("score: %.2lf\n", scoreCPU1);
+    printf("value: %.2lf\n", valueCPU1);
+    printf("efficiency: %.2lf\n", efficiencyCPU1);
+    hashSeperator(size);
+    printf("CPU: %s\n", cpu[1].name);
+    printf("score: %.2lf\n", scoreCPU2);
+    printf("value: %.2lf\n", valueCPU2);
+    printf("efficiency: %.2lf\n", efficiencyCPU2);
+    hashSeperator(size);
 
     return 0;
 }
 
-double calculateScore(CPU *ptr, double weight) {
+double calculateScore1(CPU *cpu1, double weight) {
 
-    double result = (ptr->clock * ptr->cores) + (ptr->l3cache * weight);
+    double result1 = (cpu1->clock * cpu1->cores) + (cpu1->l3cache * weight);
 
-    return result;
+    return result1;
 }
 
-void hashSeperator() {
-    for (int i = 0; i < 44; i++) {
-	printf("#");
+double calculateScore2(CPU *cpu2, double weight) {
+
+    double result2 = (cpu2->clock * cpu2->cores) + (cpu2->l3cache * weight);
+
+    return result2;
+}
+
+void hashSeperator(int size) {
+
+    for (int i = 0; i < size; i++) {
+	    printf("#");
     }
     printf("\n");
 }
