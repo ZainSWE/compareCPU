e3da2631bca1be5ef201876899ea3e17cfe5820a
Zain Ahmad <zainahmad.swe@gmail.com>
2025-10-19 08:45:04 -0400

    added value & efficiency metrics

--- main.c
+++ main.c
@@ -19,26 +19,28 @@ int main () {
     CPU *ptr = &cpu[0];
 
     strcpy (cpu[0].name, "null");
-    cpu[0].clock = 3.5;
-    cpu[0].cache = 0.0;
-    cpu[0].price = 0.0;
-    cpu[0].threads = 0;
-    cpu[0].cores = 6;
-    cpu[0].tdp = 0;
+    cpu[0].clock = 3.6;
+    cpu[0].cache = 25.0;
+    cpu[0].price = 573.52;
+    cpu[0].threads = 20;
+    cpu[0].cores = 12;
+    cpu[0].tdp = 125;
 
 
     double score = calculateScore(ptr);
     double value = score / cpu[0].price;
     double efficiency = score / cpu[0].tdp;
 
-    printf("score: %.1lf\n", score);
+    printf("score: %.2lf\n", score);
+    printf("value: %.2lf\n", value);
+    printf("efficiency: %.2lf\n", efficiency);
 
     return 0;
 }
 
 double calculateScore(CPU *ptr) {
 
-    double result = ptr->clock * ptr->cores;
+    double result = (ptr->clock * ptr->cores * 1.0) + (ptr->cache * 0.5) + (ptr->cache * 0.3);
 
     return result;
 }
